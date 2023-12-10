86397c2346af1698c0552fab682f8c707d360c0e
naji-alhusami <najihussami@gmail.com>
2023-12-10 16:10:18 +0300

    added main navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Navbar /> */}
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/places/new" element={<NewPlace />} />
-        <Route path="/*" element={<Navigate to="/" replace />} />
-        {/* <Route element={<RequireAuth />}> */}
-        {/* <Route path="/addplace" element={<AddPlace />} /> */}
-        {/* </Route> */}
-      </Routes>
-      {/* <Footer /> */}
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="/*" element={<Navigate to="/" replace />} />
+          {/* <Route element={<RequireAuth />}> */}
+          {/* <Route path="/addplace" element={<AddPlace />} /> */}
+          {/* </Route> */}
+        </Routes>
+        {/* <Footer /> */}
+      </main>
     </BrowserRouter>
   );
 }
