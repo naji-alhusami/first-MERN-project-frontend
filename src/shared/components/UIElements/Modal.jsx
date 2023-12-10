1cac9ba08d0039383e7283f340951268c9ee8011
naji-alhusami <najihussami@gmail.com>
2023-12-10 17:18:27 +0300

    added Modal to show map

---
+++ src/shared/components/UIElements/Modal.jsx
@@ -0,0 +1,56 @@
+import React, { useRef } from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+
+import Backdrop from "./Backdrop";
+import "./Modal.css";
+
+const ModalOverlay = (props) => {
+  const content = (
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        onSubmit={
+          props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
+        }
+      >
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
+        </div>
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>
+  );
+  return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
+};
+
+const Modal = (props) => {
+  const nodeRef = useRef(null);
+
+  return (
+    <React.Fragment>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <CSSTransition
+        ref={nodeRef}
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames="modal"
+      >
+        {/* <div
+          ref={props.nodeRef}
+          className={`modal ${props.className} style={props.style}`}
+        > */}
+        <ModalOverlay {...props} />
+        {/* </div> */}
+      </CSSTransition>
+    </React.Fragment>
+  );
+};
+
+export default Modal;
