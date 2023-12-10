b92fd5825e4e83f69f08393617522316215f9270
naji-alhusami <najihussami@gmail.com>
2023-12-10 14:31:05 +0300

    added the users page

---
+++ src/user/components/UsersList.jsx
@@ -0,0 +1,33 @@
+import Card from "../../shared/components/UIElements/Card";
+import UserItem from "./UserItem";
+import "./UsersList.css";
+
+const UsersList = (props) => {
+  const { items } = props;
+
+  if (items.length === 0) {
+    return (
+      <div>
+        <Card>
+          <h2>No Users Found</h2>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="users-list">
+      {items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default UsersList;
--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -1,5 +1,24 @@
-function Users() {
-  return <h1>Users Page</h1>;
-}
+import UsersList from "../components/UsersList";
+
+import naji from "../../assets/naji.jpeg";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "Naji",
+      image: naji,
+      places: 3,
+    },
+    {
+      id: "u2",
+      name: "Naji1",
+      image: naji,
+      places: 2,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
